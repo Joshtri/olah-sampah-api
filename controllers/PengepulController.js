ba6ea09a2234bf467833d2a2b8bdfed211401cfa
Joshtri <stuffofyos1516@gmail.com>
2024-12-05 20:21:02 +0800

    lts

--- controllers/PengepulController.js
+++ controllers/PengepulController.js
@@ -23,4 +23,21 @@ export class PengepulController {
             res.status(400).json({ success: false, message: error.message });
         }
     }
+
+    async getPengepulById(req, res) {
+        const { id } = req.params;
+    
+        try {
+          const pengepul = await this.pengepulService.getPengepulById(id);
+          res.status(200).json({
+            success: true,
+            data: pengepul,
+          });
+        } catch (error) {
+          res.status(404).json({
+            success: false,
+            message: error.message,
+          });
+        }
+    }
 }
